5e601b80d95d596279aed28a38075831f976fef6
Air <air-lidon@hotmail.com>
2020-12-12 21:18:55 +0700

    init

---
+++ Coin2/Coin2.ino
@@ -0,0 +1,66 @@
+/*
+ * 
+ * 25วินาที
+ * 1บาทรีเลย์ทำงาน 25วินาที
+ * 1บาท 1พัล
+ * 
+ */
+
+void ICACHE_RAM_ATTR doCounter();
+
+const int Relay1 = D0; // กำหนดขาใช้งาน
+const int sensor = D1; 
+
+boolean isCounter = false;
+int count = 0;
+int countBuf = 0;
+int countDelay = 0;
+const int dalayVal = 250; // milisec
+
+void setup() {
+  pinMode(Relay1, OUTPUT); // กำหนดขาทำหน้าที่ให้ขา D0 เป็น OUTPUT
+  digitalWrite(Relay1, HIGH);
+  pinMode(sensor, INPUT);
+  attachInterrupt(digitalPinToInterrupt(sensor), doCounter, FALLING);
+  Serial.begin(9600);
+  
+  Serial.println("======================");
+  Serial.print("จำนวนเงิน: ");
+  Serial.print(countBuf);
+  Serial.println(" บาท");
+  Serial.println();
+}
+
+void loop() {
+  if(countBuf != count) {
+    countBuf = count;
+    Serial.print("จำนวนเงิน: ");
+    Serial.print(countBuf);
+    Serial.println(" บาท");
+    Serial.println();
+    Serial.printf("countDelay: %d \r\n", countDelay);
+  }
+
+  if(countDelay) {
+    countDelay -= dalayVal;
+    digitalWrite(Relay1, LOW); //close, on
+
+    if(countDelay) {
+      int modulo = countDelay % (dalayVal*100);
+      if(!modulo) count--;  
+    } else {
+      count = 0;
+    }  
+    
+  } else {   
+    digitalWrite(Relay1, HIGH); //open, off
+  }
+
+  delay(dalayVal);
+
+}
+
+void doCounter() {
+  count++; 
+  countDelay = countDelay+(dalayVal*100); // 1บาทรีเลย์ทำงาน 25วินาที 
+}
